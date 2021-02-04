091b4972d633b9d14c972766d22878bbf6de18e4
hassan-aboueisha <32465625+hassan-aboueisha@users.noreply.github.com>
2021-02-04 07:24:17 +0200

    Solution for hackerRank array-ds challenge
    This is a solution for the coding challenge https://www.hackerrank.com/challenges/arrays-ds/ at hackerrank.

---
+++ arrays-ds.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the reverseArray function below.
+function reverseArray(a) {
+    for (var i = 0;i * 2 < a.length;i++) {
+        var temp = a[i];
+        a[i] = a[a.length - 1 - i];
+        a[a.length - 1 - i] = temp;
+    }
+    return a;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arrCount = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const res = reverseArray(arr);
+
+    ws.write(res.join(' ') + '\n');
+
+    ws.end();
+}
